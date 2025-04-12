7568856ad5c2293530a79d325d605431561d11c6
M50505 <94233844+M50505@users.noreply.github.com>
2025-04-12 19:25:53 -0400

    Corrected broken span

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Documentation Root <span className="text-[hsl(280,100%,70%)]">/span>
+          <span>Documentation Root <span className="text-[hsl(280,100%,70%)]"/span>
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
@@ -16,8 +16,7 @@ export default function HomePage() {
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
             href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
+            target="_blank">
 
             <h3 className="text-2xl font-bold">Documentation →</h3>
             <div className="text-lg">
