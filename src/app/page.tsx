dd0657fd179712dc3d35da5e83b3c612833288bd
Martin Cribbins <M50505@stonefifteen.com>
2025-04-12 18:25:49 -0400

    Corrected splash text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Documentation Root <span className="text-[hsl(280,100%,70%)]">/span> App
+          Documentation Root <span className="text-[hsl(280,100%,70%)]">/span>
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
@@ -18,7 +18,7 @@ export default function HomePage() {
             href="https://create.t3.gg/en/introduction"
             target="_blank"
           >
-            
+
             <h3 className="text-2xl font-bold">Documentation →</h3>
             <div className="text-lg">
               Go to the AsciiDoc-based sections, each from selected Git repositories, that Antora generated into
